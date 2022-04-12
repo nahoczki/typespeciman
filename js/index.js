bdab61a3b1533edd8a16c30e4a03d545a498f4ca
nahoczki <nnahoczki@gmail.com>
2022-04-12 14:50:44 -0500

    Added breakpoints, replaced lorum ipsum, final

--- js/index.js
+++ js/index.js
@@ -12,12 +12,42 @@ window.onload = () => {
     console.log("loaded")
 
     let charsRef = document.getElementById("characters")
+    let numsRef = document.getElementById("numbers")
+    let symbolsRef = document.getElementById("symbols")
+
 
     for (let i = 65; i <= 90; i++) {
         charsRef.innerHTML += "<div class=\"letterBox\">\n" +
                         `        <p class=\"regular\">&#${i}&#${97 + (i - 65)}</p>\n` +
                         "     </div>"
     }
+
+    for (let i = 48; i <= 57; i++) {
+        numsRef.innerHTML += "<div class=\"letterBox\">\n" +
+            `        <p class=\"regular\">&#${i}</p>\n` +
+            "     </div>"
+    }
+
+    for (let i = 33; i <= 39; i++) {
+        if (i === 39) {
+            i = 63;
+        }
+        symbolsRef.innerHTML += "<div class=\"letterBox\">\n" +
+            `        <p class=\"regular\">&#${i}</p>\n` +
+            "     </div>"
+    }
+
+    symbolsRef.innerHTML += "<div class=\"letterBox\">\n" +
+        `        <p class=\"regular\">&#${46}</p>\n` +
+        "     </div>"
+
+    symbolsRef.innerHTML += "<div class=\"letterBox\">\n" +
+        `        <p class=\"regular\">&#${123}</p>\n` +
+        "     </div>"
+
+    symbolsRef.innerHTML += "<div class=\"letterBox\">\n" +
+        `        <p class=\"regular\">&#${125}</p>\n` +
+        "     </div>"
 
     document.addEventListener("scroll", scrollFunc);
 }
